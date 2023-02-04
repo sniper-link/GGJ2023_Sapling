65361ed0aded43d0f474a351c04f1f29c89c4588
Igor <98488444+nampacc@users.noreply.github.com>
2023-02-03 21:21:34 -0800

    enemy

--- GGJ2023_Sapling_Unity/Assets/Igor/EnemyScript.cs
+++ GGJ2023_Sapling_Unity/Assets/Igor/EnemyScript.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float health;
+    public float damage;
+    public CircleCollider2D cc;
+    public Rigidbody2D rb;
+    public Vector2 saplingLoc;
+    float moveSpeed = 1;
+
+    private void Awake()
     {
         
+        SpriteRenderer sprRend;
+        cc = gameObject.GetComponent<CircleCollider2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+
     }
+    void Start()
+    {
+        saplingLoc = GameObject.Find("TheSapling").transform.position;
+    }
+
+    float dealDamange(float d) 
+    {
+        d = damage;
+        return d;  
+    }
+
+    void moveToSapling(Vector2 location) {
+        transform.position = Vector2.MoveTowards(transform.position, location, 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out TheSapling sapling)) {
+            //change back to float
+            sapling.TakeDamage((int)damage);
+           // sapling.takeDamage(damage);
+        }
+        
+    }
+    // Start is called before the first frame update
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveToSapling(saplingLoc);
     }
+
 }
