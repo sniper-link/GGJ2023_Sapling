3e542fef4e824c33928b5284ce316edbf6840960
Adam Mendoza <adammendoza27@gmail.com>
2023-02-04 21:38:14 -0800

    Update OnDrag.cs

--- GGJ2023_Sapling_Unity/Assets/Adam/OnDrag.cs
+++ GGJ2023_Sapling_Unity/Assets/Adam/OnDrag.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnDrag : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+     public void OnDrag(PointerEventData eventData)
+     {
+         if (!beingPlaced) return;
+ 
+         if (beingPlaced == true)
+         {
+             var pos = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0f));
+             t$$anonymous$$s.transform.position = new Vector3(pos.x, pos.y, 0);
+         }
+     }
+ 
+     public void OnEndDrag(PointerEventData eventData)
+     {
+         if (!beingPlaced) return;
+ 
+         if (cannotPlaceTower == false || collisionCount == 0)
+         {
+             gameController.placingTower = false;
+             uiController.cancelTowerButton.SetActive(false);
+             gameController.gameMusic.PlayOneShot(gameController.towerPlace, 1f);
+             beingPlaced = false; //place tower
+             towerRange.$$anonymous$$deRangeCircle();
+             gameController.addEnergy(-towerCost);            
+         }
+     }
+ 
+     public void OnPointerDown(PointerEventData eventData)
+     {
+         if (beingPlaced == false)
+         {
+             if (gameController.placingTower)
+                 gameController.stopTowerBeingPlaced();
+             calculateDPS();
+             uiController.towerPanel.GetComponent<TowerPanelController>().showTowerPanel(t$$anonymous$$s);
+         }
+     }
