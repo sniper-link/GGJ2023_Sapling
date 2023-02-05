b019eef7caaa9c4553e5ff3b806a40dcd226d44f
sniper-link <44655165+sniper-link@users.noreply.github.com>
2023-02-04 23:22:56 -0800

    comment out onDrag

--- GGJ2023_Sapling_Unity/Assets/Adam/OnDrag.cs
+++ GGJ2023_Sapling_Unity/Assets/Adam/OnDrag.cs
@@ -1,40 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-     public void OnDrag(PointerEventData eventData)
-     {
-         if (!beingPlaced) return;
- 
-         if (beingPlaced == true)
-         {
-             var pos = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0f));
-             t$$anonymous$$s.transform.position = new Vector3(pos.x, pos.y, 0);
-         }
-     }
- 
-     public void OnEndDrag(PointerEventData eventData)
-     {
-         if (!beingPlaced) return;
- 
-         if (cannotPlaceTower == false || collisionCount == 0)
-         {
-             gameController.placingTower = false;
-             uiController.cancelTowerButton.SetActive(false);
-             gameController.gameMusic.PlayOneShot(gameController.towerPlace, 1f);
-             beingPlaced = false; //place tower
-             towerRange.$$anonymous$$deRangeCircle();
-             gameController.addEnergy(-towerCost);            
-         }
-     }
- 
-     public void OnPointerDown(PointerEventData eventData)
-     {
-         if (beingPlaced == false)
-         {
-             if (gameController.placingTower)
-                 gameController.stopTowerBeingPlaced();
-             calculateDPS();
-             uiController.towerPanel.GetComponent<TowerPanelController>().showTowerPanel(t$$anonymous$$s);
-         }
-     }
+public class OnDrag : MonoBehaviour//, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    /*public void OnDrag(PointerEventData eventData)
+    {
+        if (!beingPlaced) return;
+
+        if (beingPlaced == true)
+        {
+            var pos = Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, 0f));
+            t$$anonymous$$s.transform.position = new Vector3(pos.x, pos.y, 0);
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (!beingPlaced) return;
+
+        if (cannotPlaceTower == false || collisionCount == 0)
+        {
+            gameController.placingTower = false;
+            uiController.cancelTowerButton.SetActive(false);
+            gameController.gameMusic.PlayOneShot(gameController.towerPlace, 1f);
+            beingPlaced = false; //place tower
+            towerRange.$$anonymous$$deRangeCircle();
+            gameController.addEnergy(-towerCost);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (beingPlaced == false)
+        {
+            if (gameController.placingTower)
+                gameController.stopTowerBeingPlaced();
+            calculateDPS();
+            uiController.towerPanel.GetComponent<TowerPanelController>().showTowerPanel(t$$anonymous$$s);
+        }
+    }*/
+}
